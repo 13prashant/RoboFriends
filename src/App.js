f538dd537c732e8f99e92ab72c43fdd19d302d94
Prashant Patel <pacific1392@gmail.com>
2021-05-25 20:11:04 +0530

    Implemented Redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
 import CardList from './components/CardList'
 import SearchBox from './components/SearchBox'
 import './App.css';
 import Scroll from './components/Scroll'
 import ErrorBoundary from './components/ErrorBoundary'
 
+import { setSearchField } from './actions'
 
-
-const App = () => {
-
-    const [searchField, setSearchField] = useState('')
-    const [robots, setRobots] = useState([])
-
-    const searchChange = (e) => {
-        setSearchField(e.target.value)
+const mapStateToProps = (state) => {
+    return {
+        searchField: state.searchField
     }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        handleSearchChange: (e) => dispatch(setSearchField(e.target.value))
+    }
+}
+
+const App = (props) => {
+
+    const [robots, setRobots] = useState([])
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users => setRobots(users))
     }, [])
+
+    const { searchField, handleSearchChange } = props
 
     const filteredRobots = robots.filter(robot =>
         robot.name.toLowerCase().includes(searchField.toLowerCase())
@@ -31,7 +41,7 @@ const App = () => {
             <h1 className='tc'>Loading...</h1> :
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={searchChange} />
+                <SearchBox searchChange={handleSearchChange} />
                 <Scroll>
                     <ErrorBoundary>
                         <CardList robots={filteredRobots} />
@@ -41,4 +51,4 @@ const App = () => {
     )
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
